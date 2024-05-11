c0e054cee29ad413ba5b52262c7f48801b57d899
USIsolutions <121586538+USIsolutions@users.noreply.github.com>
2024-05-10 23:35:36 -0400

    EOD Commit

---
+++ BAPD_Main_GUI.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'BAPD_Main_GUI.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QFrame, QLabel,
+    QMainWindow, QMenu, QMenuBar, QPlainTextEdit,
+    QPushButton, QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 817)
+        font = QFont()
+        font.setFamilies([u"Comic Sans MS"])
+        font.setPointSize(12)
+        font.setBold(True)
+        MainWindow.setFont(font)
+        MainWindow.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.compileButton = QPushButton(self.centralwidget)
+        self.compileButton.setObjectName(u"compileButton")
+        self.compileButton.setGeometry(QRect(360, 120, 140, 50))
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.compileButton.sizePolicy().hasHeightForWidth())
+        self.compileButton.setSizePolicy(sizePolicy)
+        self.compileButton.setFont(font)
+        self.editSourceButton = QPushButton(self.centralwidget)
+        self.editSourceButton.setObjectName(u"editSourceButton")
+        self.editSourceButton.setGeometry(QRect(40, 120, 140, 50))
+        sizePolicy.setHeightForWidth(self.editSourceButton.sizePolicy().hasHeightForWidth())
+        self.editSourceButton.setSizePolicy(sizePolicy)
+        self.editSourceButton.setFont(font)
+        self.editSourceButton.setCursor(QCursor(Qt.IBeamCursor))
+        self.runCurrentButton = QPushButton(self.centralwidget)
+        self.runCurrentButton.setObjectName(u"runCurrentButton")
+        self.runCurrentButton.setGeometry(QRect(520, 120, 140, 50))
+        sizePolicy.setHeightForWidth(self.runCurrentButton.sizePolicy().hasHeightForWidth())
+        self.runCurrentButton.setSizePolicy(sizePolicy)
+        self.runCurrentButton.setFont(font)
+        self.mameDebugCheckBox = QCheckBox(self.centralwidget)
+        self.mameDebugCheckBox.setObjectName(u"mameDebugCheckBox")
+        self.mameDebugCheckBox.setGeometry(QRect(670, 120, 81, 50))
+        self.mameDebugCheckBox.setFont(font)
+        self.versionPushButton = QPushButton(self.centralwidget)
+        self.versionPushButton.setObjectName(u"versionPushButton")
+        self.versionPushButton.setGeometry(QRect(619, 700, 161, 24))
+        font1 = QFont()
+        font1.setFamilies([u"Comic Sans MS"])
+        font1.setPointSize(9)
+        font1.setBold(False)
+        self.versionPushButton.setFont(font1)
+        self.versionPushButton.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.versionPushButton.setFlat(True)
+        self.viewListingButton = QPushButton(self.centralwidget)
+        self.viewListingButton.setObjectName(u"viewListingButton")
+        self.viewListingButton.setGeometry(QRect(200, 120, 140, 50))
+        sizePolicy.setHeightForWidth(self.viewListingButton.sizePolicy().hasHeightForWidth())
+        self.viewListingButton.setSizePolicy(sizePolicy)
+        self.viewListingButton.setFont(font)
+        self.viewListingButton.setCursor(QCursor(Qt.IBeamCursor))
+        self.clearScreenButton = QPushButton(self.centralwidget)
+        self.clearScreenButton.setObjectName(u"clearScreenButton")
+        self.clearScreenButton.setGeometry(QRect(310, 690, 191, 51))
+        font2 = QFont()
+        font2.setFamilies([u"Comic Sans MS"])
+        font2.setPointSize(10)
+        font2.setBold(True)
+        self.clearScreenButton.setFont(font2)
+        self.clearScreenButton.setAutoDefault(True)
+        self.runStandardMameButton = QPushButton(self.centralwidget)
+        self.runStandardMameButton.setObjectName(u"runStandardMameButton")
+        self.runStandardMameButton.setGeometry(QRect(30, 690, 75, 51))
+        font3 = QFont()
+        font3.setFamilies([u"Comic Sans MS"])
+        font3.setPointSize(8)
+        font3.setBold(True)
+        self.runStandardMameButton.setFont(font3)
+        self.runStandardMameButton.setAutoDefault(True)
+        self.selectProjectButton = QPushButton(self.centralwidget)
+        self.selectProjectButton.setObjectName(u"selectProjectButton")
+        self.selectProjectButton.setGeometry(QRect(30, 30, 75, 61))
+        font4 = QFont()
+        font4.setFamilies([u"Comic Sans MS"])
+        font4.setPointSize(11)
+        font4.setBold(True)
+        font4.setStyleStrategy(QFont.PreferAntialias)
+        self.selectProjectButton.setFont(font4)
+        self.openProjectFolderButton = QPushButton(self.centralwidget)
+        self.openProjectFolderButton.setObjectName(u"openProjectFolderButton")
+        self.openProjectFolderButton.setGeometry(QRect(690, 30, 75, 61))
+        self.openProjectFolderButton.setFont(font2)
+        self.fileNameLabel = QLabel(self.centralwidget)
+        self.fileNameLabel.setObjectName(u"fileNameLabel")
+        self.fileNameLabel.setGeometry(QRect(128, 30, 531, 61))
+        font5 = QFont()
+        font5.setFamilies([u"Comic Sans MS"])
+        font5.setPointSize(16)
+        font5.setBold(True)
+        self.fileNameLabel.setFont(font5)
+        self.fileNameLabel.setFrameShape(QFrame.Shape.Box)
+        self.fileNameLabel.setFrameShadow(QFrame.Shadow.Plain)
+        self.fileNameLabel.setLineWidth(2)
+        self.fileNameLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.setGeometry(QRect(23, 200, 751, 471))
+        font6 = QFont()
+        font6.setFamilies([u"Consolas"])
+        font6.setPointSize(12)
+        font6.setBold(False)
+        self.plainTextEdit.setFont(font6)
+        self.plainTextEdit.setReadOnly(True)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.menuAbout = QMenu(self.menubar)
+        self.menuAbout.setObjectName(u"menuAbout")
+        self.menuAbout.setFont(font)
+        self.menuAbout.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        self.menuSettings = QMenu(self.menubar)
+        self.menuSettings.setObjectName(u"menuSettings")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuAbout.menuAction())
+        self.menubar.addAction(self.menuSettings.menuAction())
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"BAPD - Bally Astrocade Program Development", None))
+        self.compileButton.setText(QCoreApplication.translate("MainWindow", u"Compile", None))
+        self.editSourceButton.setText(QCoreApplication.translate("MainWindow", u"Edit Source", None))
+        self.runCurrentButton.setText(QCoreApplication.translate("MainWindow", u"Run Current", None))
+        self.mameDebugCheckBox.setText(QCoreApplication.translate("MainWindow", u"MAME \n"
+"Debug", None))
+        self.versionPushButton.setText(QCoreApplication.translate("MainWindow", u"Version: 20240427", None))
+        self.viewListingButton.setText(QCoreApplication.translate("MainWindow", u"View Listing ", None))
+        self.clearScreenButton.setText(QCoreApplication.translate("MainWindow", u"Clear\n"
+"Screen", None))
+        self.runStandardMameButton.setText(QCoreApplication.translate("MainWindow", u"Run\n"
+"Standard\n"
+"MAME", None))
+        self.selectProjectButton.setText(QCoreApplication.translate("MainWindow", u"Select\n"
+"Project", None))
+        self.openProjectFolderButton.setText(QCoreApplication.translate("MainWindow", u"Open\n"
+"Project\n"
+"Folder", None))
+        self.fileNameLabel.setText(QCoreApplication.translate("MainWindow", u"Astrocade_Program.asm", None))
+        self.menuAbout.setTitle(QCoreApplication.translate("MainWindow", u"About", None))
+        self.menuSettings.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
+    # retranslateUi
+
---
+++ BAPD_Settings_GUI.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'BAPD_Settings_GUI.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+                            QMetaObject, QObject, QPoint, QRect,
+                            QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+                           QFont, QFontDatabase, QGradient, QIcon,
+                           QImage, QKeySequence, QLinearGradient, QPainter,
+                           QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QDialog, QFrame,
+                               QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+                               QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+
+
+class Ui_BAPD_Settings(object):
+    def setupUi(self, BAPD_Settings):
+        if not BAPD_Settings.objectName():
+            BAPD_Settings.setObjectName(u"BAPD_Settings")
+        BAPD_Settings.resize(912, 669)
+        self.zmacSettingsLayout = QGroupBox(BAPD_Settings)
+        self.zmacSettingsLayout.setObjectName(u"zmacSettingsLayout")
+        self.zmacSettingsLayout.setGeometry(QRect(20, 30, 871, 541))
+        font = QFont()
+        font.setPointSize(14)
+        self.zmacSettingsLayout.setFont(font)
+        self.zmacSettingsLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.zmacSettingsLayout.setFlat(False)
+        self.optionsGroupBox = QGroupBox(self.zmacSettingsLayout)
+        self.optionsGroupBox.setObjectName(u"optionsGroupBox")
+        self.optionsGroupBox.setGeometry(QRect(620, 90, 231, 361))
+        self.verticalLayoutWidget = QWidget(self.optionsGroupBox)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(20, 40, 202, 301))
+        self.zmacOptionsVerticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.zmacOptionsVerticalLayout.setObjectName(u"zmacOptionsVerticalLayout")
+        self.zmacOptionsVerticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.expandIncludeFiles = QCheckBox(self.verticalLayoutWidget)
+        self.expandIncludeFiles.setObjectName(u"expandIncludeFiles")
+        font1 = QFont()
+        font1.setPointSize(12)
+        self.expandIncludeFiles.setFont(font1)
+
+        self.zmacOptionsVerticalLayout.addWidget(self.expandIncludeFiles)
+
+        self.expandMacros = QCheckBox(self.verticalLayoutWidget)
+        self.expandMacros.setObjectName(u"expandMacros")
+        self.expandMacros.setFont(font1)
+
+        self.zmacOptionsVerticalLayout.addWidget(self.expandMacros)
+
+        self.useUndocumentedInstructions = QCheckBox(self.verticalLayoutWidget)
+        self.useUndocumentedInstructions.setObjectName(u"useUndocumentedInstructions")
+        self.useUndocumentedInstructions.setFont(font1)
+
+        self.zmacOptionsVerticalLayout.addWidget(self.useUndocumentedInstructions)
+
+        self.outputHexFile = QCheckBox(self.verticalLayoutWidget)
+        self.outputHexFile.setObjectName(u"outputHexFile")
+        self.outputHexFile.setFont(font1)
+
+        self.zmacOptionsVerticalLayout.addWidget(self.outputHexFile)
+
+        self.omitSymbolTable = QCheckBox(self.verticalLayoutWidget)
+        self.omitSymbolTable.setObjectName(u"omitSymbolTable")
+        self.omitSymbolTable.setFont(font1)
+
+        self.zmacOptionsVerticalLayout.addWidget(self.omitSymbolTable)
+
+        self.labelsMustHaveColons = QCheckBox(self.verticalLayoutWidget)
+        self.labelsMustHaveColons.setObjectName(u"labelsMustHaveColons")
+        self.labelsMustHaveColons.setFont(font1)
+
+        self.zmacOptionsVerticalLayout.addWidget(self.labelsMustHaveColons)
+
+        self.zmacVersionButton = QLabel(self.zmacSettingsLayout)
+        self.zmacVersionButton.setObjectName(u"zmacVersionButton")
+        self.zmacVersionButton.setGeometry(QRect(620, 490, 231, 31))
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.zmacVersionButton.sizePolicy().hasHeightForWidth())
+        self.zmacVersionButton.setSizePolicy(sizePolicy)
+        font2 = QFont()
+        font2.setPointSize(11)
+        self.zmacVersionButton.setFont(font2)
+        self.zmacVersionButton.setFrameShape(QFrame.Shape.Box)
+        self.zmacVersionButton.setScaledContents(False)
+        self.zmacVersionButton.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.zmacFilePathLayout = QGroupBox(self.zmacSettingsLayout)
+        self.zmacFilePathLayout.setObjectName(u"zmacFilePathLayout")
+        self.zmacFilePathLayout.setGeometry(QRect(10, 90, 571, 161))
+        self.zmacFilePathLayout.setAlignment(
+            Qt.AlignmentFlag.AlignLeading | Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        self.horizontalLayoutWidget = QWidget(self.zmacFilePathLayout)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(10, 30, 551, 111))
+        self.zmacPathInputLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.zmacPathInputLayout.setObjectName(u"zmacPathInputLayout")
+        self.zmacPathInputLayout.setContentsMargins(0, 0, 0, 0)
+        self.zmacPathLabel = QLabel(self.horizontalLayoutWidget)
+        self.zmacPathLabel.setObjectName(u"zmacPathLabel")
+
+        self.zmacPathInputLayout.addWidget(self.zmacPathLabel)
+
+        self.zmacPathLineEdit = QLineEdit(self.horizontalLayoutWidget)
+        self.zmacPathLineEdit.setObjectName(u"zmacPathLineEdit")
+
+        self.zmacPathInputLayout.addWidget(self.zmacPathLineEdit)
+
+        self.browseZmacButton = QPushButton(self.horizontalLayoutWidget)
+        self.browseZmacButton.setObjectName(u"browseZmacButton")
+        self.browseZmacButton.setFont(font1)
+        self.browseZmacButton.setAutoDefault(True)
+
+        self.zmacPathInputLayout.addWidget(self.browseZmacButton)
+
+        self.retranslateUi(BAPD_Settings)
+
+        QMetaObject.connectSlotsByName(BAPD_Settings)
+
+    # setupUi
+
+    def retranslateUi(self, BAPD_Settings):
+        BAPD_Settings.setWindowTitle(QCoreApplication.translate("BAPD_Settings", u"Settings", None))
+        self.zmacSettingsLayout.setTitle(QCoreApplication.translate("BAPD_Settings", u"ZMAC Settings", None))
+        self.optionsGroupBox.setTitle(QCoreApplication.translate("BAPD_Settings", u"Options", None))
+        self.expandIncludeFiles.setText(QCoreApplication.translate("BAPD_Settings", u"Expand Include Files", None))
+        self.expandMacros.setText(QCoreApplication.translate("BAPD_Settings", u"Expand Macros", None))
+        self.useUndocumentedInstructions.setText(QCoreApplication.translate("BAPD_Settings", u"Use Undocumented \n"
+                                                                                             "Z80 Instructions", None))
+        self.outputHexFile.setText(QCoreApplication.translate("BAPD_Settings", u"Output .hex File", None))
+        self.omitSymbolTable.setText(QCoreApplication.translate("BAPD_Settings", u"Omit Symbol Table", None))
+        self.labelsMustHaveColons.setText(QCoreApplication.translate("BAPD_Settings", u"Labels Must Have Colons", None))
+        self.zmacVersionButton.setText(QCoreApplication.translate("BAPD_Settings", u"ZMAC Version 3.1", None))
+        self.zmacFilePathLayout.setTitle(QCoreApplication.translate("BAPD_Settings", u"File Locations", None))
+        self.zmacPathLabel.setText(QCoreApplication.translate("BAPD_Settings", u"ZMAC Path:", None))
+        self.browseZmacButton.setText(QCoreApplication.translate("BAPD_Settings", u"Browse...", None))
+    # retranslateUi
